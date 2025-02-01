0251731a2c5cf8b0ed97b7e0070a189e19853a7b
lacerda <edulacerdaaa@gmail.com>
2025-02-01 18:58:54 -0300

    atualiza exemplo de votação com loop do-while, adiciona contagem de votos e exibe resultados

--- iterators/while/6/script.js
+++ iterators/while/6/script.js
@@ -14,16 +14,17 @@
 // c) A porcentagem de votos brancos;
 // d) O candidato vencedor.
 
+let option
+let c1 = 0
+let c2 = 0
+let c3 = 0
+let nullvote = 0
+let whitevote = 0
 
 do {
-    let option = prompt(
+    option = prompt(
         "Realize o seu voto: \n1. Candidato Jair Rodrigues \n2. Candidato Carlos Luz \n3. Candidato Neves Rocha \n4. Nulo \n5. Branco"
     )
-    let c1 = 0
-    let c2 = 0
-    let c3 = 0
-    let nullvote = 0
-    let whitevote = 0
 
     switch (option) {
         case "1":
@@ -32,7 +33,25 @@ do {
         case "2":
             c2++
             break
+        case "3":
+            c3++
+            break
+        case "4": 
+            nullvote++
+            break
+        case "5":
+            whitevote++
+            break
+        case "6":
+            alert("encerrando sessão eleitoral..")
+            break
+        default:
+            alert("invalid input")
 
     }
 
 } while (option != "6");
+
+console.log(
+    `Apuração dos votos: \n1. ${c1} \n2. ${c2} \n3. ${c3} \n4. ${nullvote} \n5. ${whitevote}`
+)
