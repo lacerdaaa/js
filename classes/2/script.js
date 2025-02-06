dd4cd540bbc2443db91ff1faee6c150c624b4e6c
lacerdaaa <edulacerdaaa@gmail.com>
2025-02-06 11:34:20 -0300

    feat: classes exercise using bank logical

--- classes/2/script.js
+++ classes/2/script.js
@@ -1,8 +1,8 @@
 // 2. Implemente uma classe chamada “ContaBancária” que possua atributos para armazenar o número da conta, nome do titular e saldo. Adicione métodos para realizar depósitos e saques.
 
 class BankAccount {
-    constructor(agence, ownerName, balance) {
-        this.agence = agence;
+    constructor(accountNumber, ownerName, balance = 0) {
+        this.accountNumber = accountNumber;
         this.ownerName = ownerName;
         this.balance = Number(balance);
     }
@@ -26,31 +26,27 @@ class BankAccount {
     }
 }
 
-
 class Bank {
-    constructor() {}
+    constructor() { }
 
-    transference(fromAccount, toAccount, balance) {
-
+    transference(fromAccount, toAccount, amount) {
+        if (fromAccount.balance >= amount && amount > 0) {
+            toAccount.balance += amount;
+            fromAccount.balance -= amount;
+            console.log(`A $${amount.toFixed(2)} transfer was successfully made from account ${fromAccount.accountNumber} to account ${toAccount.accountNumber}.`);
+            console.log(`New balance of ${fromAccount.ownerName}: $${fromAccount.balance.toFixed(2)}`);
+            console.log(`New balance of ${toAccount.ownerName}: $${toAccount.balance.toFixed(2)}`);
+        } else {
+            console.log("Insufficient balance or invalid transfer amount.");
+        }
     }
 }
 
-class Person {
-    constructor(name, bank) {
-        this.name = name;
-        this.bank = bank;
-    }
-}
+const account1 = new BankAccount(123, 'John Doe', 1000);
+const account2 = new BankAccount(456, 'Jane Smith', 500);
 
+account1.deposit(200);
+account1.withdraw(150);
 
-
-
-
-
-
-
-const bankAccount = new BankAccount(150, "Lacerda", 100);
-const person = new Person("Eduardo", bankAccount);
-
-person.bank.deposit(100); 
-person.bank.withdraw(50); 
+const bank = new Bank();
+bank.transference(account1, account2, 300);
